2f3e351acccd079560169d4645a967313b9f575d
Arie Milner <amilner42@gmail.com>
2025-02-16 15:31:57 -0800

    Set formatter to use 120 for line length for ex and heex files

--- lib/aveline_web/telemetry.ex
+++ lib/aveline_web/telemetry.ex
@@ -70,8 +70,7 @@ defmodule AvelineWeb.Telemetry do
       ),
       summary("aveline.repo.query.idle_time",
         unit: {:native, :millisecond},
-        description:
-          "The time the connection spent waiting before being checked out for the query"
+        description: "The time the connection spent waiting before being checked out for the query"
       ),
 
       # VM Metrics
