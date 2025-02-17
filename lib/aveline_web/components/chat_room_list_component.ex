95f4f1294da81cd6d3021d6e24f175d43188fc47
Arie Milner <amilner42@gmail.com>
2025-02-17 12:00:05 -0800

    Boilerplate for a chat room list component

---
+++ lib/aveline_web/components/chat_room_list_component.ex
@@ -0,0 +1,36 @@
+defmodule AvelineWeb.ChatRoomListComponent do
+  @moduledoc """
+  This component is used to display a list of chat rooms.
+  """
+  use Phoenix.Component
+
+  attr :chat_rooms, :list, required: true
+  attr :selected_chat_room_id, :string, default: nil
+  attr :on_chat_room_click, :any, required: true
+  attr :on_new_chat_room_click, :any, required: true
+
+  def chat_room_list(assigns) do
+    ~H"""
+    <div class="flex flex-col h-full">
+      <div class="overflow-y-auto flex-1">
+        <%= for chat_room <- @chat_rooms do %>
+          <button
+            phx-click={@on_chat_room_click}
+            phx-value-id={chat_room.id}
+            class={[
+              "w-full p-4 text-left hover:bg-gray-50",
+              @selected_chat_room_id == chat_room.id && "bg-gray-100"
+            ]}
+          >
+            <div class="font-medium">{chat_room.name}</div>
+            <div class="text-sm text-gray-500">{chat_room.last_message}</div>
+          </button>
+        <% end %>
+      </div>
+      <button phx-click={@on_new_chat_room_click} class="w-full p-4 text-left hover:bg-gray-50">
+        <div class="font-medium">New Chat</div>
+      </button>
+    </div>
+    """
+  end
+end
--- lib/aveline_web/live/chat_live.ex
+++ lib/aveline_web/live/chat_live.ex
@@ -1,9 +1,19 @@
 defmodule AvelineWeb.ChatLive do
   use AvelineWeb, :live_view
+  import AvelineWeb.ChatRoomListComponent
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    harcoded_chat_rooms = [
+      %{id: "1", name: "Chat 1", last_message: "Hello, how are you?"},
+      %{id: "2", name: "Chat 2", last_message: "Hello, how are you?"},
+      %{id: "3", name: "Chat 3", last_message: "Hello, how are you?"}
+    ]
+
+    {:ok,
+     socket
+     |> assign(chat_rooms: harcoded_chat_rooms)
+     |> assign(selected_chat_room_id: nil)}
   end
 
   @impl true
@@ -11,12 +21,25 @@ defmodule AvelineWeb.ChatLive do
     ~H"""
     <div class="flex h-full w-full">
       <div class="w-full lg:w-96 border-r border-gray-200">
-        chat boxes
+        <.chat_room_list
+          chat_rooms={@chat_rooms}
+          selected_chat_room_id={@selected_chat_room_id}
+          on_chat_room_click="select_chat_room"
+          on_new_chat_room_click="new_chat_room"
+        />
       </div>
       <div class="hidden lg:block h-full flex-1">
         <h1 class="text-2xl font-bold">Chat</h1>
       </div>
     </div>
     """
+  end
+
+  def handle_event("select_chat_room", %{"id" => id}, socket) do
+    {:noreply, socket |> assign(selected_chat_room_id: id)}
+  end
+
+  def handle_event("new_chat_room", _params, socket) do
+    {:noreply, socket}
   end
 end
