37e55c74d254a1593b22c5dd071f6f4e339889e7
Arie Milner <amilner42@gmail.com>
2025-03-08 15:20:42 -0800

    Add back auto-resizing, accidentally removed that before

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -3,6 +3,7 @@ import autosize from './autosize.min.js'
 export const EnhancedTextarea = {
   mounted() {
     this.el.focus();
+    autosize(this.el);
     // Event listener to disable textarea on form submission. Because we use phx-update="ignore", Phoenix doesn't
     // automatically disable the textarea when the form is submitted.
     this.el.form.addEventListener("submit", (event) => {
@@ -17,7 +18,7 @@ export const EnhancedTextarea = {
       this.el.disabled = true;
       return true;
     });
-    // Event listener for Command+Enter to submit the form
+    // Event listener for Command+Enter to submit the form. Purposefully done in JS to be snappy.
     this.el.addEventListener("keydown", (event) => {
       if (event.key === "Enter" && event.metaKey) {
         this.el.form.dispatchEvent(new Event('submit', {bubbles: true, cancelable: true}));
