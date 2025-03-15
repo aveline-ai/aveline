23c8960fa063839c5c202d7d44adab03f0d2e764
Arie Milner <amilner42@gmail.com>
2025-03-15 14:14:13 -0700

    Add openai_ex dep

--- mix.exs
+++ mix.exs
@@ -53,7 +53,8 @@ defmodule Aveline.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:openai_ex, "~> 0.9.0"}
     ]
   end
 
